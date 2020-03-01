e7f12956803603e4932effdbce4fb6d4308bb994
nuintun <295288114@qq.com>
2020-03-01 23:09:47 +0800

    :art: Update files

--- out/src/import-sorter.js
+++ out/src/import-sorter.js
@@ -10,11 +10,11 @@ var SortConfig;
 })(SortConfig || (SortConfig = {}));
 class ImportSorter {
     constructor(config) {
+        this.config = config;
         this.defaultTabWidth = this.config.defaultTabWidth;
         this.trailingComma = this.config.trailingComma;
         this.inputPerLine = this.config.inputPerLine;
         this.lineMaxWidth = this.config.lineMaxWidth;
-        this.config = config;
     }
     // extension.sortImportLine
     sortImportLine() {
@@ -33,55 +33,53 @@ class ImportSorter {
     }
     // TODO: break up to not handle messaging
     handleSorting(format) {
-        const activeEditor = this.checkEditorAndSelection(vscode_1.window.activeTextEditor, (error) => {
-            vscode_1.window.showErrorMessage(error);
-            return;
-        });
-        if (activeEditor !== undefined) {
+        const activeEditor = this.checkEditorAndSelection(vscode_1.window.activeTextEditor, (error) => vscode_1.window.showErrorMessage(error));
+        if (activeEditor) {
             const [editor, selection] = activeEditor;
             const selectionEndLine = editor.document.lineAt(selection.end.line);
             const range = new vscode_1.Range(selection.start.line, 0, selection.end.line, selectionEndLine.range.end.character);
             const input = editor.document.getText(range);
-            if (!isImportStatement(input)) {
-                vscode_1.window.showErrorMessage(`Selection does not seem to contain import statement`);
-                return;
+            if (isImportStatement(input)) {
+                const config = {
+                    tabWidth: this.defaultTabWidth,
+                    trailingComma: this.trailingComma,
+                    inputPerLine: this.inputPerLine,
+                    lineMaxWidth: this.lineMaxWidth
+                };
+                const sortedImport = sortImportSelection(input, format, config);
+                editor.edit(builder => builder.replace(range, sortedImport));
+                vscode_1.window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
             }
-            const config = {
-                tabWidth: this.defaultTabWidth,
-                trailingComma: this.trailingComma,
-                inputPerLine: this.inputPerLine,
-                lineMaxWidth: this.lineMaxWidth
-            };
-            const sortedImport = sortImportSelection(input, format, config);
-            editor.edit(builder => builder.replace(range, sortedImport));
-            vscode_1.window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
+            else {
+                vscode_1.window.showErrorMessage(`Selection does not seem to contain import statement`);
+            }
         }
     }
     // Make sure editor is active and that there is a selection.
     checkEditorAndSelection(editor, cb) {
-        if (editor === undefined) {
+        if (!editor) {
             return cb(`Editor is not active`);
         }
-        if (editor.selection === undefined) {
+        if (!editor.selection) {
             return cb(`No valid selection`);
         }
         return [editor, editor.selection];
     }
 }
 exports.ImportSorter = ImportSorter;
-// ---------- HELP FUNCTIONS ------------------------------------------
 function isImportStatement(selection) {
     return /\b(import|export)\b(.|\s)*\bfrom\b/m.test(selection);
 }
 function sortImportSelection(selection, format, options) {
-    return selection.replace(/\{[^.]*?\}/gm, exp => {
-        const normalized = exp.trim().replace(/\s+/g, ' ');
-        const match = RegExp(/\{(.*?)\}/, 'g').exec(normalized);
-        if (match === null || match[1] === undefined) {
+    return selection
+        .trim()
+        .replace(/(\s)+/gm, '$1')
+        .replace(/\{[^.]*?\}/gm, exp => {
+        const match = /\{(.*?)\}/gm.exec(exp);
+        if (!match || !match[1])
             return exp;
-        }
         const arrayToSort = match[1].split(',').filter(n => n);
-        const sortedArray = sortArray(arrayToSort);
+        const sortedArray = sortArray(arrayToSort.map(n => n.trim()));
         return formatArray(sortedArray, format, options);
     });
 }
@@ -92,34 +90,20 @@ function formatArray(arr, format, options) {
     const { tabWidth, trailingComma, inputPerLine, lineMaxWidth } = options;
     let formattedArray;
     switch (format) {
-        case SortConfig.SingleLine: {
-            formattedArray = arr.map((entry, index) => {
-                if (index !== 0) {
-                    entry = ' ' + entry;
-                }
-                return entry;
-            });
+        case SortConfig.SingleLine:
+            formattedArray = arr.map((entry, index) => (index !== 0 ? ` ${entry}` : entry));
             return `{ ${formattedArray} }`;
-        }
-        case SortConfig.MultiLine: {
-            formattedArray = arr.map(entry => {
-                entry = '\n' + ' '.repeat(tabWidth) + entry;
-                return entry;
-            });
-            if (trailingComma === true) {
+        case SortConfig.MultiLine:
+            formattedArray = arr.map(entry => `\n${' '.repeat(tabWidth)}${entry}`);
+            if (trailingComma)
                 return `{${formattedArray},\n}`;
-            }
             return `{${formattedArray}\n}`;
-        }
-        case SortConfig.WordGroup: {
+        case SortConfig.WordGroup:
             return `{${formatLine(arr, inputPerLine, tabWidth, trailingComma)}\n}`;
-        }
-        case SortConfig.MaxWidth: {
+        case SortConfig.MaxWidth:
             return `{\n${formatLineWidth(arr, lineMaxWidth, tabWidth, trailingComma)}\n}`;
-        }
-        default: {
+        default:
             return arr.toString();
-        }
     }
 }
 function formatLine(arr, perRow, tabWidth, trailingComma) {
@@ -127,13 +111,13 @@ function formatLine(arr, perRow, tabWidth, trailingComma) {
     for (let i = 0; i < arr.length; i++) {
         const newRow = i % perRow;
         if (newRow === 0) {
-            res += '\n' + ' '.repeat(tabWidth);
+            res += `\n${' '.repeat(tabWidth)}`;
         }
         res += arr[i];
         if (i !== arr.length - 1) {
             res += ', ';
         }
-        if (i === arr.length - 1 && trailingComma === true) {
+        if (i === arr.length - 1 && trailingComma) {
             res += ',';
         }
     }
@@ -144,15 +128,15 @@ function formatLineWidth(arr, maxWidth, tabWidth, trailingComma) {
     let res = ' '.repeat(tabWidth);
     for (let i = 0; i < arr.length; i++) {
         const currentLength = arr[i].length;
-        if (lineWidth + currentLength > maxWidth && i !== 0) {
-            res += '\n' + ' '.repeat(tabWidth);
+        if (i > 0 && lineWidth + currentLength > maxWidth) {
             lineWidth = tabWidth;
+            res += `\n${' '.repeat(tabWidth)}`;
         }
         res += arr[i];
         if (i !== arr.length - 1) {
             res += ', ';
         }
-        if (i === arr.length - 1 && trailingComma === true) {
+        if (i === arr.length - 1 && trailingComma) {
             res += ',';
         }
         lineWidth += currentLength + 2;
--- src/import-sorter.ts
+++ src/import-sorter.ts
@@ -15,15 +15,12 @@ interface Options {
 }
 
 export class ImportSorter {
-  private config: WorkspaceConfiguration;
   private defaultTabWidth = this.config.defaultTabWidth;
   private trailingComma = this.config.trailingComma;
   private inputPerLine = this.config.inputPerLine;
   private lineMaxWidth = this.config.lineMaxWidth;
 
-  constructor(config: WorkspaceConfiguration) {
-    this.config = config;
-  }
+  constructor(private config: WorkspaceConfiguration) {}
 
   // extension.sortImportLine
   public sortImportLine(): void {
@@ -46,44 +43,41 @@ export class ImportSorter {
 
   // TODO: break up to not handle messaging
   private handleSorting(format: SortConfig): void {
-    const activeEditor = this.checkEditorAndSelection(window.activeTextEditor, (error: string) => {
-      window.showErrorMessage(error);
-      return;
-    });
+    const activeEditor = this.checkEditorAndSelection(window.activeTextEditor, (error: string) =>
+      window.showErrorMessage(error)
+    );
 
-    if (activeEditor !== undefined) {
+    if (activeEditor) {
       const [editor, selection] = activeEditor;
       const selectionEndLine = editor.document.lineAt(selection.end.line);
 
       const range = new Range(selection.start.line, 0, selection.end.line, selectionEndLine.range.end.character);
       const input = editor.document.getText(range);
 
-      if (!isImportStatement(input)) {
+      if (isImportStatement(input)) {
+        const config: Options = {
+          tabWidth: this.defaultTabWidth,
+          trailingComma: this.trailingComma,
+          inputPerLine: this.inputPerLine,
+          lineMaxWidth: this.lineMaxWidth
+        };
+        const sortedImport = sortImportSelection(input, format, config);
+
+        editor.edit(builder => builder.replace(range, sortedImport));
+        window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
+      } else {
         window.showErrorMessage(`Selection does not seem to contain import statement`);
-
-        return;
       }
-
-      const config: Options = {
-        tabWidth: this.defaultTabWidth,
-        trailingComma: this.trailingComma,
-        inputPerLine: this.inputPerLine,
-        lineMaxWidth: this.lineMaxWidth
-      };
-      const sortedImport = sortImportSelection(input, format, config);
-
-      editor.edit(builder => builder.replace(range, sortedImport));
-      window.showInformationMessage(`${selection.end.line - selection.start.line + 1} number of lines got sorted.`);
     }
   }
 
   // Make sure editor is active and that there is a selection.
   private checkEditorAndSelection(editor: TextEditor | undefined, cb: (err: string) => void): [TextEditor, Selection] | void {
-    if (editor === undefined) {
+    if (!editor) {
       return cb(`Editor is not active`);
     }
 
-    if (editor.selection === undefined) {
+    if (!editor.selection) {
       return cb(`No valid selection`);
     }
 
@@ -91,25 +85,24 @@ export class ImportSorter {
   }
 }
 
-// ---------- HELP FUNCTIONS ------------------------------------------
 function isImportStatement(selection: string): boolean {
   return /\b(import|export)\b(.|\s)*\bfrom\b/m.test(selection);
 }
 
 function sortImportSelection(selection: string, format: SortConfig, options: Options): string {
-  return selection.replace(/\{[^.]*?\}/gm, exp => {
-    const normalized = exp.trim().replace(/\s+/g, ' ');
-    const match = RegExp(/\{(.*?)\}/, 'g').exec(normalized);
+  return selection
+    .trim()
+    .replace(/(\s)+/gm, '$1')
+    .replace(/\{[^.]*?\}/gm, exp => {
+      const match = /\{(.*?)\}/gm.exec(exp);
 
-    if (match === null || match[1] === undefined) {
-      return exp;
-    }
+      if (!match || !match[1]) return exp;
 
-    const arrayToSort = match[1].split(',').filter(n => n);
-    const sortedArray = sortArray(arrayToSort);
+      const arrayToSort = match[1].split(',').filter(n => n);
+      const sortedArray = sortArray(arrayToSort.map(n => n.trim()));
 
-    return formatArray(sortedArray, format, options);
-  });
+      return formatArray(sortedArray, format, options);
+    });
 }
 
 function sortArray(arr: string[]): string[] {
@@ -122,43 +115,22 @@ function formatArray(arr: string[], format: SortConfig, options: Options): strin
   let formattedArray;
 
   switch (format) {
-    case SortConfig.SingleLine: {
-      formattedArray = arr.map((entry, index) => {
-        if (index !== 0) {
-          entry = ' ' + entry;
-        }
-
-        return entry;
-      });
+    case SortConfig.SingleLine:
+      formattedArray = arr.map((entry, index) => (index !== 0 ? ` ${entry}` : entry));
 
       return `{ ${formattedArray} }`;
-    }
+    case SortConfig.MultiLine:
+      formattedArray = arr.map(entry => `\n${' '.repeat(tabWidth)}${entry}`);
 
-    case SortConfig.MultiLine: {
-      formattedArray = arr.map(entry => {
-        entry = '\n' + ' '.repeat(tabWidth) + entry;
-
-        return entry;
-      });
-
-      if (trailingComma === true) {
-        return `{${formattedArray},\n}`;
-      }
+      if (trailingComma) return `{${formattedArray},\n}`;
 
       return `{${formattedArray}\n}`;
-    }
-
-    case SortConfig.WordGroup: {
+    case SortConfig.WordGroup:
       return `{${formatLine(arr, inputPerLine, tabWidth, trailingComma)}\n}`;
-    }
-
-    case SortConfig.MaxWidth: {
+    case SortConfig.MaxWidth:
       return `{\n${formatLineWidth(arr, lineMaxWidth, tabWidth, trailingComma)}\n}`;
-    }
-
-    default: {
+    default:
       return arr.toString();
-    }
   }
 }
 
@@ -169,7 +141,7 @@ function formatLine(arr: string[], perRow: number, tabWidth: number, trailingCom
     const newRow = i % perRow;
 
     if (newRow === 0) {
-      res += '\n' + ' '.repeat(tabWidth);
+      res += `\n${' '.repeat(tabWidth)}`;
     }
 
     res += arr[i];
@@ -178,7 +150,7 @@ function formatLine(arr: string[], perRow: number, tabWidth: number, trailingCom
       res += ', ';
     }
 
-    if (i === arr.length - 1 && trailingComma === true) {
+    if (i === arr.length - 1 && trailingComma) {
       res += ',';
     }
   }
@@ -193,9 +165,9 @@ function formatLineWidth(arr: string[], maxWidth: number, tabWidth: number, trai
   for (let i = 0; i < arr.length; i++) {
     const currentLength = arr[i].length;
 
-    if (lineWidth + currentLength > maxWidth && i !== 0) {
-      res += '\n' + ' '.repeat(tabWidth);
+    if (i > 0 && lineWidth + currentLength > maxWidth) {
       lineWidth = tabWidth;
+      res += `\n${' '.repeat(tabWidth)}`;
     }
 
     res += arr[i];
@@ -204,7 +176,7 @@ function formatLineWidth(arr: string[], maxWidth: number, tabWidth: number, trai
       res += ', ';
     }
 
-    if (i === arr.length - 1 && trailingComma === true) {
+    if (i === arr.length - 1 && trailingComma) {
       res += ',';
     }
 
